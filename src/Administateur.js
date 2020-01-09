578b849b9aa9b0fd5ffcb0bea2d19de1409ff635
dylan belouis <dylan.belouis@gmail.com>
2020-01-09 09:53:08 +0100

    add favicon

--- src/Administateur.js
+++ src/Administateur.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import UserIcon from '@material-ui/icons/Group';
-import { UsersList, UserCreate } from './components/users';
+import { UsersList, UserCreate, UserFilter } from './components/users';
 import Dashboard from './components/Dashboard';
 import authProvider from './components/authProvider';
 import PersistentDrawerRight from './components/PersistentDrawerRight';
+
 // import MyLoginPage from './MyLoginPage';
 // import MyLogoutButton from './MyLogoutButton';
 
@@ -21,6 +22,7 @@ const Administrateur = (props) => (
       // loginPage={MyLoginPage}
       // logoutButton={MyLogoutButton}
       name="users"
+      filter={UserFilter}
       list={UsersList}
       edit={PersistentDrawerRight}
       create={UserCreate}
