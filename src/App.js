5f0869905700a721a9cc466a906eb849783054a9
rodolphe37 <rodolphe.a@gmail.com>
2020-01-10 09:58:54 +0100

    another modificaions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
-//import jsonServerProvider from 'ra-data-json-server';
+import jsonServerProvider from 'ra-data-json-server';
 import UserIcon from '@material-ui/icons/Group';
 import { UsersList, UsersEdit, UserCreate } from './components/users';
 import Dashboard from './components/Dashboard';
 import authProvider from './components/authProvider';
 //import dataProvider from './components/dataProvider';
+// import { createMuiTheme } from '@material-ui/core/styles';
+
+
 
 const dataProvider = jsonServerProvider(process.env.REACT_APP_API_URL);
 
 const App = () => (
-  <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider} >
+  <Admin  dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider} >
     <Resource name="users" list={UsersList} edit={UsersEdit} create={UserCreate} icon={UserIcon} />
   </Admin>
 );
