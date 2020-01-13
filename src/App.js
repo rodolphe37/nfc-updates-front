eb323c90e63e77b387c5ce809dd5923ed60b7ee3
rodolphe37 <rodolphe.a@gmail.com>
2020-01-13 22:56:09 +0100

    refactoring and install query-string

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import { Admin, Resource } from 'react-admin';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import jsonServerProvider from 'ra-data-json-server';
 import UserIcon from '@material-ui/icons/Group';
-// import CardActions from '@material-ui/core/CardActions';
-// import Button from '@material-ui/core/Button';
 import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { UsersList, UsersEdit, UserCreate } from './components/users';
-// import Dashboard from './components/Dashboard';
 import authProvider from './components/authProvider';
-// import ButtonToggleTheme from './ButtonToggleTheme';
 import Dashboard from './components/dashboard/Dashboard';
 
 const dataProvider = jsonServerProvider(process.env.REACT_APP_API_URL);
@@ -36,15 +31,26 @@ const App = () => {
   // the mui theme is used in the themeProvider.
   return (
     <MuiThemeProvider theme={muiTheme}>
-      {/* <FormControlLabel
-        control={<Switch onChange={toggleDarkTheme} aria-label="Change Themes" />}
-      /> */}
-      <Admin dashboard={Dashboard} theme={muiTheme} authProvider={authProvider} dataProvider={dataProvider}>
-        <Switch onChange={toggleDarkTheme} aria-label="Change Themes" />
-        <Resource name="users" list={UsersList} edit={UsersEdit} create={UserCreate} icon={UserIcon} />
+      <Admin
+        dashboard={Dashboard}
+        theme={muiTheme}
+        authProvider={authProvider}
+        dataProvider={dataProvider}
+      >
+        <Switch
+          onChange={toggleDarkTheme}
+          aria-label="Change-themes"
+        />
+        <Resource
+          name="users"
+          list={UsersList}
+          edit={UsersEdit}
+          create={UserCreate}
+          icon={UserIcon}
+        />
       </Admin>
     </MuiThemeProvider>
   );
 };
-// dashboard={Dashboard}
+
 export default App;
