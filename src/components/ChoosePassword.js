63d93e3c08595c60f528ed71b825fb14c9bbc8bb
rodolphe37 <rodolphe.a@gmail.com>
2020-01-20 09:33:26 +0100

    debuging choosePassword component

--- src/components/ChoosePassword.js
+++ src/components/ChoosePassword.js
@@ -18,7 +18,6 @@ const theme2 = createMuiTheme({
         backgroundColor: '#f50057',
         borderRadius: 3,
         border: 0,
-        // textColor: 'red',
         height: 48,
         padding: '0 30px',
         boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
