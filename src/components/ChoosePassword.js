05c6f7fab0251c0110696ff43cc92130d14235ae
rodolphe37 <rodolphe.a@gmail.com>
2020-01-20 09:25:50 +0100

    create button and theme in ChoosePassword component

--- src/components/ChoosePassword.js
+++ src/components/ChoosePassword.js
@@ -6,44 +6,67 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { TextInput } from 'react-admin';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme2 = createMuiTheme({
+  overrides: {
+    // Style sheet name ⚛️
+    MuiButton: {
+      // Name of the rule
+      text: {
+        // Some CSS
+        backgroundColor: '#f50057',
+        borderRadius: 3,
+        border: 0,
+        // textColor: 'red',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      },
+    },
+  },
+});
 
 
-export default function ChoosePassword({ open, handleClose }) {
+export default function ChoosePassword({ open, handleClose, theme }) {
   return (
     <div>
-
-      <Dialog open={open} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Create Password</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
+      <ThemeProvider theme={theme}>
+        <Dialog open={open} aria-labelledby="form-dialog-title">
+          <DialogTitle id="form-dialog-title">Create Password</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
             To create your password, please enter an new password here.
-          </DialogContentText>
-          <TextInput
-            autoFocus
-            margin="dense"
-            label="password"
-            type="password"
-            fullWidth
-            source="password"
-          />
-          <TextInput
-            autoFocus
-            margin="dense"
-            label="password"
-            type="password"
-            fullWidth
-            source="password"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
+            </DialogContentText>
+            <TextInput
+              autoFocus
+              margin="dense"
+              label="password"
+              type="password"
+              fullWidth
+              source="password"
+            />
+            <TextInput
+              autoFocus
+              margin="dense"
+              label="password"
+              type="password"
+              fullWidth
+              source="password"
+            />
+          </DialogContent>
+          <DialogActions>
+            <ThemeProvider theme={theme2}>
+              <Button onClick={handleClose} color="primary">
             Cancel
-          </Button>
-          <Button onClick={handleClose} color="primary">
+              </Button>
+              <Button onClick={handleClose} color="primary">
             Save Password
-          </Button>
-        </DialogActions>
-      </Dialog>
+              </Button>
+            </ThemeProvider>
+          </DialogActions>
+        </Dialog>
+      </ThemeProvider>
     </div>
   );
 }
