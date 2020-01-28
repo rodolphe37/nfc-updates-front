43bcc6393aabec98f31af5a355bad646386b2717
rodolphe37 <rodolphe.a@gmail.com>
2020-01-28 17:49:36 +0100

    debug save button on modale page

--- src/components/ChoosePassword.js
+++ src/components/ChoosePassword.js
@@ -9,16 +9,40 @@ import {
   TextInput, SimpleForm, required, SaveButton, Toolbar, useRedirect,
 } from 'react-admin';
 import IconCancel from '@material-ui/icons/Cancel';
-import { ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
+const theme2 = createMuiTheme({
+  overrides: {
+    // Style sheet name ⚛️
+    MuiButton: {
+      // Name of the rule
+      text: {
+        // Some CSS
+        background: '#f50057',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      },
+    },
+  },
+});
 
-export default function ChoosePassword({ onCancel, open, theme }) {
+export default function ChoosePassword({
+  onCancel, open, theme, handleClose,
+}) {
   const redirect = useRedirect();
 
   const PasswordEditToolbar = ({ ...props }) => (
     <Toolbar basepath="true" {...props}>
       <DialogActions saving="true">
-        <SaveButton {...props} handlesubmitwithredirect />
+        <ThemeProvider theme={theme2}>
+          <Button onClick={handleClose} color="primary" required emptyValue={null}>
+      Save Password
+          </Button>
+        </ThemeProvider>
       </DialogActions>
       <DialogActions>
         <Button label="ra.action.cancel" onClick={() => redirect('/users')}>
